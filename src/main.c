7b13c0a751f4bec456304240b8f8edb7f54369ef
Hélio Santos <128417028+Helio0408@users.noreply.github.com>
2023-10-31 21:21:10 -0300

    sort retirado da main e add comentarios

--- src/main.c
+++ src/main.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-#include <string.h>
 #include "process.h"
 
 int main(void){
 	char operacao[7] = "", descricao[MAX_DESCR]= "", opcao[3] = "";
 	int prior, hh, mm, ss, prior_n, HH, MM, SS;
-	bool sort_by_priority;
+	bool sort_by_priority = 0;
 	int algo_foi_printado = 0;
 	horario tempo, tempo_n;
 	lista *l;
@@ -13,14 +11,16 @@ int main(void){
 	l = lista_criar();
 
 	while(1){
-		scanf("%s ", operacao);
+		scanf("%s ", operacao); // escolha da operação
 
+		// adiciona um processo à lista
 		if(strcmp(operacao, "add") == 0){
 			scanf("%d %d:%d:%d %s", &prior, &hh, &mm, &ss, descricao);
 			tempo = horario_criar(hh, mm, ss);
 
 			lista_add(l, celula_criar(prior, tempo, descricao));
 
+		// remove o processo de maior prioridade ou de menor tempo
 		}else if(strcmp(operacao, "exec") == 0){
 			scanf("%s", opcao);
 
@@ -29,9 +29,9 @@ int main(void){
 			else
 				sort_by_priority = false;
 			
-			lista_sort(l, sort_by_priority);
-			lista_exec(l);
+			lista_exec(l, sort_by_priority);
 
+		// printa o processo de maior prioridade ou de menor tempo
 		}else if(strcmp(operacao, "next") == 0){
 			if(!algo_foi_printado) algo_foi_printado = 1;
 			else printf("\r\n");
@@ -43,21 +43,17 @@ int main(void){
 			else
 				sort_by_priority = false;
 			
-			lista_sort(l, sort_by_priority);
-			lista_next(l);
+			lista_next(l, sort_by_priority);
 
+		// muda a prioridade ou o tempo de um processo
 		}else if(strcmp(operacao, "change") == 0){
 			scanf("%s", opcao);
 
 			if(strcmp(opcao, "-p") == 0){
-				lista_sort(l, 1);
-
 				scanf("%d|%d", &prior, &prior_n);
 
 				lista_modificaPrioridade(l, prior, prior_n);
 			}else{
-				lista_sort(l, 0);
-
 				scanf("%d:%d:%d|%d:%d:%d", &hh, &mm, &ss, &HH, &MM, &SS);
 				tempo = horario_criar(hh, mm, ss);
 				tempo_n = horario_criar(HH, MM, SS);
@@ -65,6 +61,7 @@ int main(void){
 				lista_modificaTempo(l, tempo, tempo_n);
 			}
 
+		// printa todos os termos em ordem decrescente de acordo com a opção
 		}else if(strcmp(operacao, "print") == 0){
 			if(!algo_foi_printado) algo_foi_printado = 1;
 			else printf("\r\n");
@@ -76,9 +73,9 @@ int main(void){
 			else
 				sort_by_priority = 0;
 			
-			lista_sort(l, sort_by_priority);
-			lista_print(l);
-			
+			lista_print(l, sort_by_priority);
+
+		// sai do while e termina o programa	
 		}else if(strcmp(operacao, "quit") == 0){
 			break;
 		}
